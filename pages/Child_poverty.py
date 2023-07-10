c7f6d7be25ed3b34f4103942052d4b2035f9129c
celhakiza <hakizacel@gmail.com>
2023-07-10 20:48:41 +0200

    added code for chained callback

--- pages/Child_poverty.py
+++ pages/Child_poverty.py
@@ -1,57 +1,85 @@
 #import useful libraries
 import dash
-from dash.dependencies import Output, Input
 import pandas as pd
-from dash import html,callback
 import dash_bootstrap_components as dbc
-from dash import dcc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import callback
+from dash.dependencies import Output, Input
 import plotly.express as px
 
-#import dataset
+# Create the Dash app
 dash.register_page(__name__,order=5)
+# Import dataset
 df_child_poverty = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\children_poverty.csv')
-layout = dbc.Container([
-    dbc.Row(html.H2('Trend in Child poverty in Rwanda',className="text-center text-primary mb-4 font-weight-bold")),
+df_child_moda_4 = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\moda_4.csv')
+df_child_moda_7 = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\moda_7.csv')
 
+# Layout
+layout = dbc.Container([
+    dbc.Row(html.H2('Trend in Child poverty in Rwanda', className="text-center text-primary mb-4 font-weight-bold")),
     dbc.Row([
         dbc.Col([
-            html.H4('Children Monetary Poverty',
-                    style={'color':'blue'}),
-            html.Div('Select gender:',className="text-primary mb-4 font-weight-bold"),
-            dcc.Checklist(id='pov',
-                          value=['Male'],
-                          options=[{'label':x,'value':x} for x in df_child_poverty['Sex'].unique()]),
-
-            dcc.Graph(id='pove-graph',
-                      style = {'width':'500px',
-                               'height':'300px'}),
-            html.P(['Children poverty trends shows at which percentage are the population under the age of 18 who are below the \n'
-                   'poverty line as the data from', html.Mark("EICV"), 'shows. the data are available for two EICV: one conducted in', html.Mark("2013/2014"), 'and the \n'
-                   'other conducted in', html.Mark("2016/2017"),'.'])
-        ], width=6),
-
+            html.H4('Children Monetary Poverty', style={'color': 'blue'}),
+            html.Div('Select gender:', className="text-primary mb-4 font-weight-bold"),
+            dcc.Checklist(
+                id='pov',
+                value=['Male'],
+                options=[{'label': x, 'value': x} for x in df_child_poverty['Sex'].unique()]
+            ),
+            dcc.Graph(id='pove-graph', style={'width': '500px', 'height': '300px'}),
+            html.P([
+                'Children poverty trends shows at which percentage are the population under the age of 18 who are below the ',
+                html.Mark("poverty line"), ' as the data from', html.Mark("EICV"),
+                ' shows. The data are available for two EICV: one conducted in', html.Mark("2013/2014"),
+                ' and the other conducted in', html.Mark("2016/2017"), '.']
+            )
+        ], width=5, style={'margin-right': '20px'}),
         dbc.Col([
-            html.H4('Multidimensional Overlapping Deprivation Analysis (MODA)',
-                    style={'color':'blue'})
-        ],width=6)
+            html.H4('Multidimensional Overlapping Deprivation Analysis (MODA)', style={'color': 'blue'}),
+            dcc.Dropdown(
+                id='moda',
+                value='Multidimensional Child poverty for children aged 5 - 14',
+                options=[
+                    'Multidimensional Child poverty for children aged 5 - 14',
+                    'Multidimensional Child poverty for children aged 15 - 17'
+                ],
+                style={
+                    'width': '85%',
+                    'font-size': '15px',
+                    'margin-bottom': '20px',
+                    'font-weight': 'lighter'
+                }
+            ),
+            dcc.Graph(id='moda-graph', figure={}),
+            html.P(
+                'The graph shows the comparison of MODA for the dimensions of Housing, Health, Sanitation, Education, and Water. '
+                'It compares data between EICV 2013/2014 and 2016/2017',
+                style={
+                    'font-family': 'Times New Roman',
+                    'font-size': '15px',
+                    'font-style': 'italic'
+                }
+            )
+        ], width=6)
     ])
 ])
+
+
 @callback(
-    Output(component_id='pove-graph',component_property='figure'),
-    Input(component_id='pov',component_property='value')
+    Output(component_id='pove-graph', component_property='figure'),
+    Input(component_id='pov', component_property='value')
 )
 def pov(selesex):
-    if len(selesex)==0:
+    if len(selesex) == 0:
         return dash.no_update
     else:
         dff_poverty = df_child_poverty[df_child_poverty['Sex'].isin(selesex)]
-        fig = px.line(dff_poverty,
-                      x='year',
-                      y='percentage',
-                      color='Sex')
-        fig.update_layout(xaxis_title='Year', yaxis_title='Percentage',title='Trend in Children poverty')
-        fig.update_traces(showlegend=True)
+        fig = px.line(dff_poverty, x='year', y='percentage', color='Sex')
         fig.update_layout(
+            xaxis_title='Year',
+            yaxis_title='Percentage',
+            title='Trend in Children poverty',
             title_font_color='blue',
             yaxis_range=[44, 49],
             font_color='blue',
@@ -59,5 +87,25 @@ def pov(selesex):
             legend_title_font_color='blue',
             title_font_family='Arial',
             showlegend=False,
-            plot_bgcolor='white')
+            plot_bgcolor='white'
+        )
         return fig
+
+
+@callback(
+    Output('moda-graph', 'figure'),
+    Input('moda', 'value')
+)
+def moda_pov(slctind):
+    if len(slctind) == 0:
+        return dash.no_update
+    elif slctind == 'Multidimensional Child poverty for children aged 5 - 14':
+        fig = px.bar(df_child_moda_4, x='deprivation', y='percentage', barmode='group',color='year')
+        return fig
+    elif slctind == 'Multidimensional Child poverty for children aged 15 - 17':
+        fig = px.bar(df_child_moda_7, x='deprivation', y='percentage', barmode='group',color='year')
+        return fig
+
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
--- pages/Learning.py
+++ pages/Learning.py
@@ -1,207 +1,207 @@
-#import useful libraries
-import dash
-from dash.dependencies import Output, Input
-import pandas as pd
-from dash import html,callback
-import dash_bootstrap_components as dbc
-from dash import dcc
-import plotly.express as px
-
-dash.register_page(__name__,order=4)
-
-#import datasets
-df_organized = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\df_learning.csv') #organized learning
-df_school_infr = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\df_schools_infr.csv') #schools infrastructures
-df_primary = pd.read_excel(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\currently_attending_school_6_11.xlsx') #primary students
-df_secondary = pd.read_excel(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\currently_attending_school_12_17.xlsx')
-layout = dbc.Container([
-    dbc.Row([
-        html.H2('Education related indicators',className="text-center text-primary mb-4 font-weight-bold")
-    ]),
-    dbc.Row([
-        dbc.Col([
-            html.Label('Early Primary Education)',className="text-center text-primary mb-4 font-weight-bold"),
-            dcc.Dropdown(id='learning',
-                         value=['Both'],
-                         multi=True,
-                         options=[{'label':x,'value':x} for x in df_organized['Sex'].unique()],className='w-50'),
-            dcc.Graph(id='graph-learning',figure={},
-                      style = {'width':'500px','height':'300px'})
-        ],width=6),
-
-        dbc.Col([
-            html.Label('Access to infrastructures for Schools',className="text-center text-primary mb-4 font-weight-bold"),
-            html.Br(),
-            html.I('Here you find indicators related to: Access to electricity, computers, \n'
-                   'handwashing facilities and internet for nursery, primary and secondary education.'),
-            html.Br(),
-            dcc.Dropdown(id='infr',
-                         value=['Proportion of schools with access to electricity - Nursery'],
-                         multi=True,
-                         options=[{'label':x,'value':x} for x in df_school_infr['indicator'].unique()]),
-            dcc.Graph(id='graph-infr',figure={},
-                      style = {'width':'600px','height':'300px'})
-
-        ])
-    ,
-    dcc.Markdown('Source: MINEDUC',className="text-center text-primary mb-4 font-weight-bold")
-    ]),
-    html.Hr(),
-    dbc.Row([
-        html.Div('Education',className='text-center text-success mb-4 font-weight-bold')
-    ]),
-    dbc.Row([
-        dbc.Col([
-            html.Div('Select district',className="text-center text-primary mb-4 font-weight-bold"),
-            html.Div('Primary Education',className="text-center text-primary mb-4 font-weight-bold"),
-            dcc.Dropdown(id='district',
-                         value='Bugesera',
-                         options=[{'label':x,'value':x} for x in sorted(df_primary['district'].unique())],className='text-primary w-25'),
-            html.Div(id='sent-provi'),
-            dcc.Graph(id='graph-pr',figure={},
-                      style = {'width':'400px','height':'300px'})
-
-        ],width=6),
-
-        dbc.Col([
-            html.Div('Secondary Education ',className="text-center text-primary mb-4 font-weight-bold"),
-            html.Div(id='sent-sec'),
-            dcc.Graph(id='graph-sec',figure={},
-                      style = {'width':'400px','height':'300px'}),
-            dcc.Markdown('Source: Fifth Rwanda population and Housing census',className="text-center text-primary mb-4 font-weight-bold")
-        ],width=6)
-    ])
-
-])
-
-@callback(
-    Output('graph-learning','figure'),
-    Input('learning','value')
-)
-def learning(slctlearn):
-    if len(slctlearn) ==0:
-        return dash.no_update
-    else:
-        df_organized_df = df_organized[df_organized['Sex'].isin(slctlearn)]
-        fig = px.bar(df_organized_df,
-                     x='year',
-                     y='percentage',
-                     barmode='group',
-                     color='Sex')
-        fig.update_layout(xaxis_title='Year',yaxis_title='Percentage (%)',title='Early Primary education')
-        fig.update_layout(
-            title_font_color='blue',
-            font_color='blue',
-            font_family='Times New Roman',
-            legend_title_font_color='blue',
-            title_font_family='Arial',
-            showlegend=False,
-            plot_bgcolor='white')
-        return fig
-@callback(
-    Output('graph-infr','figure'),
-    Input('infr','value')
-)
-def infr(slctinf):
-    if len(slctinf) ==0:
-        return dash.no_update
-    else:
-        df_school_inf_df =df_school_infr[df_school_infr['indicator'].isin(slctinf)]
-        fig=px.line(df_school_inf_df,
-                    x='year',
-                    y='percentage',
-                    color='indicator')
-        fig.update_layout(xaxis_title='Year',yaxis_title='Percentage (%)',title='Access to infrastructures for Schools')
-        fig.update_traces(showlegend=False)
-        fig.update_layout(
-            title_font_color='blue',
-            font_color='blue',
-            font_family='Times New Roman',
-            legend_title_font_color='blue',
-            title_font_family='Arial',
-            showlegend=False,
-            plot_bgcolor='white')
-        return fig
-@callback(
-    Output('sent-provi','children'),
-    Output('graph-pr','figure'),
-    Input('district','value')
-)
-def update_graph_pri(selected_distr):
-    if len(selected_distr) == 0:
-        return dash.no_update
-    else:
-        dff_pop_prim = df_primary[(df_primary['district'] == selected_distr)].sum().squeeze()[2:5]
-        # convert series into a dataframe
-        dff_pop_prim = pd.DataFrame(dff_pop_prim)
-        # reset index of a dataframe
-        dff_pop_prim = dff_pop_prim.reset_index()
-        # rename the columns of a dataframe
-        dff_pop_prim = dff_pop_prim.rename(columns={'index': 'Sex', 0: 'percentage'})
-
-        fig = px.bar(dff_pop_prim,
-                     x='Sex',
-                     y='percentage',
-                     text='percentage',
-                     color='Sex')
-        fig.update_layout(yaxis_title='Percentage',title='Percentage of 6-11 age in primary school in {} district'.format(selected_distr))
-
-        fig.update_traces(showlegend=False)
-        fig.update_layout(
-            title_font_color='blue',
-            font_color='blue',
-            font_family='Times New Roman',
-            legend_title_font_color='blue',
-            title_font_family='Arial',
-            showlegend=False,
-            plot_bgcolor='white')
-
-        return (('In {} district, {} percent of children between\n'
-                    ' 6-11 years enrolled in primary education, female are {}\n'
-                    ' percent while male are {} percent.\n'
-                 ''.format(selected_distr,round(df_primary[df_primary['district']==selected_distr].squeeze()[2],1),
-                           round(df_primary[df_primary['district']==selected_distr].squeeze()[4],1),
-                           round(df_primary[df_primary['district']==selected_distr].squeeze()[3],1))),fig)
-
-@callback(
-    Output('sent-sec','children'),
-    Output('graph-sec','figure'),
-    Input('district','value')
-)
-def update_graph_sec(select):
-    if len(select) == 0:
-        return dash.no_update
-
-    else:
-        dff_pop_sec = df_secondary[(df_secondary['district'] == select)].sum().squeeze()[2:5]
-        # convert series into a dataframe
-        dff_pop_sec = pd.DataFrame(dff_pop_sec)
-        # reset index of a dataframe
-        dff_pop_sec = dff_pop_sec.reset_index()
-        # rename the columns of a dataframe
-        dff_pop_sec = dff_pop_sec.rename(columns={'index': 'Sex', 0: 'percentage'})
-
-        fig = px.bar(dff_pop_sec,
-                     x='Sex',
-                     y='percentage',
-                     text='percentage',
-                     color='Sex')
-        fig.update_layout(yaxis_title='Percentage',
-                          title='Percentage of 12-17 age in secondary school in {} district'.format(select))
-        fig.update_traces(showlegend=False)
-        fig.update_layout(
-            title_font_color='blue',
-            font_color='blue',
-            font_family='Times New Roman',
-            legend_title_font_color='blue',
-            title_font_family='Arial',
-            showlegend=False,
-            plot_bgcolor='white')
-
-        return (('In {} district, {} percent of children between\n'
-                 ' 12-17 years enrolled in secondary education, female are {}\n'
-                 ' percent while male are {} percent.\n'
-                 ''.format(select, round(df_secondary[df_secondary['district'] == select].squeeze()[2], 1),
-                           round(df_secondary[df_secondary['district'] == select].squeeze()[4], 1),
-                           round(df_secondary[df_secondary['district'] == select].squeeze()[3],1))), fig)
-
+# #import useful libraries
+# import dash
+# from dash.dependencies import Output, Input
+# import pandas as pd
+# from dash import html,callback
+# import dash_bootstrap_components as dbc
+# from dash import dcc
+# import plotly.express as px
+#
+# dash.register_page(__name__,order=4)
+#
+# #import datasets
+# df_organized = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\df_learning.csv') #organized learning
+# df_school_infr = pd.read_csv(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\df_schools_infr.csv') #schools infrastructures
+# df_primary = pd.read_excel(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\currently_attending_school_6_11.xlsx') #primary students
+# df_secondary = pd.read_excel(r'C:\Users\ENVY\PycharmProjects\Unicef_dashboard\EICV dash\currently_attending_school_12_17.xlsx')
+# layout = dbc.Container([
+#     dbc.Row([
+#         html.H2('Education related indicators',className="text-center text-primary mb-4 font-weight-bold")
+#     ]),
+#     dbc.Row([
+#         dbc.Col([
+#             html.Label('Early Primary Education)',className="text-center text-primary mb-4 font-weight-bold"),
+#             dcc.Dropdown(id='learning',
+#                          value=['Both'],
+#                          multi=True,
+#                          options=[{'label':x,'value':x} for x in df_organized['Sex'].unique()],className='w-50'),
+#             dcc.Graph(id='graph-learning',figure={},
+#                       style = {'width':'500px','height':'300px'})
+#         ],width=6),
+#
+#         dbc.Col([
+#             html.Label('Access to infrastructures for Schools',className="text-center text-primary mb-4 font-weight-bold"),
+#             html.Br(),
+#             html.I('Here you find indicators related to: Access to electricity, computers, \n'
+#                    'handwashing facilities and internet for nursery, primary and secondary education.'),
+#             html.Br(),
+#             dcc.Dropdown(id='infr',
+#                          value=['Proportion of schools with access to electricity - Nursery'],
+#                          multi=True,
+#                          options=[{'label':x,'value':x} for x in df_school_infr['indicator'].unique()]),
+#             dcc.Graph(id='graph-infr',figure={},
+#                       style = {'width':'600px','height':'300px'})
+#
+#         ])
+#     ,
+#     dcc.Markdown('Source: MINEDUC',className="text-center text-primary mb-4 font-weight-bold")
+#     ]),
+#     html.Hr(),
+#     dbc.Row([
+#         html.Div('Education',className='text-center text-success mb-4 font-weight-bold')
+#     ]),
+#     dbc.Row([
+#         dbc.Col([
+#             html.Div('Select district',className="text-center text-primary mb-4 font-weight-bold"),
+#             html.Div('Primary Education',className="text-center text-primary mb-4 font-weight-bold"),
+#             dcc.Dropdown(id='district',
+#                          value='Bugesera',
+#                          options=[{'label':x,'value':x} for x in sorted(df_primary['district'].unique())],className='text-primary w-25'),
+#             html.Div(id='sent-provi'),
+#             dcc.Graph(id='graph-pr',figure={},
+#                       style = {'width':'400px','height':'300px'})
+#
+#         ],width=6),
+#
+#         dbc.Col([
+#             html.Div('Secondary Education ',className="text-center text-primary mb-4 font-weight-bold"),
+#             html.Div(id='sent-sec'),
+#             dcc.Graph(id='graph-sec',figure={},
+#                       style = {'width':'400px','height':'300px'}),
+#             dcc.Markdown('Source: Fifth Rwanda population and Housing census',className="text-center text-primary mb-4 font-weight-bold")
+#         ],width=6)
+#     ])
+#
+# ])
+#
+# @callback(
+#     Output('graph-learning','figure'),
+#     Input('learning','value')
+# )
+# def learning(slctlearn):
+#     if len(slctlearn) ==0:
+#         return dash.no_update
+#     else:
+#         df_organized_df = df_organized[df_organized['Sex'].isin(slctlearn)]
+#         fig = px.bar(df_organized_df,
+#                      x='year',
+#                      y='percentage',
+#                      barmode='group',
+#                      color='Sex')
+#         fig.update_layout(xaxis_title='Year',yaxis_title='Percentage (%)',title='Early Primary education')
+#         fig.update_layout(
+#             title_font_color='blue',
+#             font_color='blue',
+#             font_family='Times New Roman',
+#             legend_title_font_color='blue',
+#             title_font_family='Arial',
+#             showlegend=False,
+#             plot_bgcolor='white')
+#         return fig
+# @callback(
+#     Output('graph-infr','figure'),
+#     Input('infr','value')
+# )
+# def infr(slctinf):
+#     if len(slctinf) ==0:
+#         return dash.no_update
+#     else:
+#         df_school_inf_df =df_school_infr[df_school_infr['indicator'].isin(slctinf)]
+#         fig=px.line(df_school_inf_df,
+#                     x='year',
+#                     y='percentage',
+#                     color='indicator')
+#         fig.update_layout(xaxis_title='Year',yaxis_title='Percentage (%)',title='Access to infrastructures for Schools')
+#         fig.update_traces(showlegend=False)
+#         fig.update_layout(
+#             title_font_color='blue',
+#             font_color='blue',
+#             font_family='Times New Roman',
+#             legend_title_font_color='blue',
+#             title_font_family='Arial',
+#             showlegend=False,
+#             plot_bgcolor='white')
+#         return fig
+# @callback(
+#     Output('sent-provi','children'),
+#     Output('graph-pr','figure'),
+#     Input('district','value')
+# )
+# def update_graph_pri(selected_distr):
+#     if len(selected_distr) == 0:
+#         return dash.no_update
+#     else:
+#         dff_pop_prim = df_primary[(df_primary['district'] == selected_distr)].sum().squeeze()[2:5]
+#         # convert series into a dataframe
+#         dff_pop_prim = pd.DataFrame(dff_pop_prim)
+#         # reset index of a dataframe
+#         dff_pop_prim = dff_pop_prim.reset_index()
+#         # rename the columns of a dataframe
+#         dff_pop_prim = dff_pop_prim.rename(columns={'index': 'Sex', 0: 'percentage'})
+#
+#         fig = px.bar(dff_pop_prim,
+#                      x='Sex',
+#                      y='percentage',
+#                      text='percentage',
+#                      color='Sex')
+#         fig.update_layout(yaxis_title='Percentage',title='Percentage of 6-11 age in primary school in {} district'.format(selected_distr))
+#
+#         fig.update_traces(showlegend=False)
+#         fig.update_layout(
+#             title_font_color='blue',
+#             font_color='blue',
+#             font_family='Times New Roman',
+#             legend_title_font_color='blue',
+#             title_font_family='Arial',
+#             showlegend=False,
+#             plot_bgcolor='white')
+#
+#         return (('In {}, {} percent of children between\n'
+#                     ' 6-11 years enrolled in primary education, female are {}\n'
+#                     ' percent while male are {} percent.\n'
+#                  ''.format(selected_distr,round(df_primary[df_primary['district']==selected_distr].squeeze()[2],1),
+#                            round(df_primary[df_primary['district']==selected_distr].squeeze()[4],1),
+#                            round(df_primary[df_primary['district']==selected_distr].squeeze()[3],1))),fig)
+#
+# @callback(
+#     Output('sent-sec','children'),
+#     Output('graph-sec','figure'),
+#     Input('district','value')
+# )
+# def update_graph_sec(select):
+#     if len(select) == 0:
+#         return dash.no_update
+#
+#     else:
+#         dff_pop_sec = df_secondary[(df_secondary['district'] == select)].sum().squeeze()[2:5]
+#         # convert series into a dataframe
+#         dff_pop_sec = pd.DataFrame(dff_pop_sec)
+#         # reset index of a dataframe
+#         dff_pop_sec = dff_pop_sec.reset_index()
+#         # rename the columns of a dataframe
+#         dff_pop_sec = dff_pop_sec.rename(columns={'index': 'Sex', 0: 'percentage'})
+#
+#         fig = px.bar(dff_pop_sec,
+#                      x='Sex',
+#                      y='percentage',
+#                      text='percentage',
+#                      color='Sex')
+#         fig.update_layout(yaxis_title='Percentage',
+#                           title='Percentage of 12-17 age in secondary school in {} district'.format(select))
+#         fig.update_traces(showlegend=False)
+#         fig.update_layout(
+#             title_font_color='blue',
+#             font_color='blue',
+#             font_family='Times New Roman',
+#             legend_title_font_color='blue',
+#             title_font_family='Arial',
+#             showlegend=False,
+#             plot_bgcolor='white')
+#
+#         return (('In {}, {} percent of children between\n'
+#                  ' 12-17 years enrolled in secondary education, female are {}\n'
+#                  ' percent while male are {} percent.\n'
+#                  ''.format(select, round(df_secondary[df_secondary['district'] == select].squeeze()[2], 1),
+#                            round(df_secondary[df_secondary['district'] == select].squeeze()[4], 1),
+#                            round(df_secondary[df_secondary['district'] == select].squeeze()[3],1))), fig)
+#
